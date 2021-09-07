69e5906eaf3ef11cde60fa7ed1ca2928b76727a5
Berta LI <you@example.com>
2021-09-07 09:37:42 +0200

    first

---
+++ main.js
@@ -0,0 +1,5 @@
+function makeShoppingList(item1, item2, item3){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
